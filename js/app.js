d729bd821433188903c3122e18fd9e55f9a2f02a
Olamide Akomolafe <olamideakomolafe1234@gmail.com>
2018-11-01 07:00:16 +0000

    Made changes to the folder structure

--- js/app.js
+++ js/app.js
@@ -6,3 +6,11 @@ function myFunction() {
       x.className = "topnav";
   }
 }
+function openNav() {
+    document.getElementById("mySidenav").style.display = "block";
+}
+
+/* Close/hide the sidenav */
+function closeNav() {
+    document.getElementById("mySidenav").style.display = "none";
+}
